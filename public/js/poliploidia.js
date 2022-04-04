81e047085d55fc172e7a959868f95931da07cd4b
201710028 <201710028@uesb.edu.br>
2022-04-04 16:07:05 -0300

    módulo de poliploidia, modificações no js

--- public/js/poliploidia.js
+++ public/js/poliploidia.js
@@ -7,10 +7,10 @@ $(document).ready(function($) {
             for(i=0;i<n;i++){
                 $('#frequencias').append("<div class='form-group col-md-6'>" +
                                             "<strong>frequência  de "+Alelos[i]+" <span style='color: red;'>*</span></strong>"+ 
-                                            "<input type='text' autocomplete='off' max='1' min='0' name='"+Alelos[i]+"' id='"+Alelos[i]+"' class='form-control' >"+ 
+                                            "<input type='text' autocomplete='off' max='1' min='0' name='"+Alelos[i]+"' id='"+Alelos[i]+"' class='form-control frequencia'>"+ 
                                             "<span class='invalid-feedback' role='alert'> <strong ></strong></span>"+
                                          "</div>");
-                                        $("#"+Alelos[i]).mask("9.9");
+                                        $("#"+Alelos[i]).mask("9.999");
             }
         }else{
             swal("Atenção!", "Selecione a quantidade de alelos","error");
@@ -39,49 +39,60 @@ $(document).ready(function($) {
 
     //o expoente na equação sempre será 2 (x + y + z)^2
     $(document).on('click','.btnCalcular', function(){
+        var valorTotal='';
+        // var quantidadeAlelos = parseInt($("#frequencias .frequencia").length);
         var n = parseInt(document.querySelector('#grau').value);
         if(n==0){
             swal("Atenção!", "Selecione o campo Grau de Ploidia (N)","error")
         }else {
-            if(parseFloat(dominante.value)+parseFloat(recessivo.value )==1 ){
+            for(var i=0;i<n;i++){
+                //console.log(valorTotal);
+                valorTotal=round(valorTotal,document.getElementById(Alelos[i]).value);
+                //console.log(round(parseFloat(document.getElementById(Alelos[i])),3));
+            }
+                
+            
+                console.log(valorTotal,'esse aqui é ofinal');
+            if(valorTotal==1){
                 $('#resultado').empty();
-                var p = parseFloat(dominante.value);
-                var q = parseFloat(recessivo.value);
-                let expoente_p = n;
-                let expoente_q = 0;
                 var resultado = '';
                 //criação de cada termo da equação  e o valor dele, por exemplo 2pq = valor (2*p*q)
-                for(i=n;i>=0;i--){
-                    
-                    resultado+= combinatoria (n,expoente_q)
+                for(i=0;i<n;i++){
+                    let frequenciaBase = parseFloat(document.querySelector('#'+Alelos[i]).value);
+                    for(k=i+1;k<n;k++){
+                        let Proximafrequencia = parseFloat(document.querySelector('#'+Alelos[k]).value);
+                        resultado+= (k == i+1 ?  
+                                                Alelos[i]+'<sup>2</sup> = '+  (frequenciaBase*frequenciaBase).toFixed(3) +'</br>'
+                                                :  '2'+Alelos[i]+Alelos[k]+' = '+(2*frequenciaBase*Proximafrequencia).toFixed(3)+'</br>' )
 
-                    resultado+= (expoente_p >0 ?  'p<sup>'+expoente_p+'</sup>' :'')
-                    
-                    resultado+= (expoente_q >0 ? 'q<sup>'+expoente_q+'</sup>'  :'')
-                    
-                    resultado+= (' = '+((p**expoente_p) * (q**expoente_q)).toFixed(3) +'</br>')
-
-                    
-                    expoente_p--;
-                    expoente_q++;
+                    }
                 }
                 $('#resultado').append(resultado);
             }else{
-                document.querySelector('#p').value='';
-                document.querySelector('#q').value='';
-                swal("Atenção!", "p + q maior ou menor que 1, verifique os campos (p) e (q)", "error");
+                swal("Atenção!", "As somas das frequências (p+q+r+...+z) está menor ou maior que 1", "error");
             }
         }
 
            
     })
-
-    function fatorial(fator){
-        if(fator <= 1)
-            return  1;
-        else
-         return fator*fatorial(fator-1);
+    
+    //função criada por causa do problema de somas das casas decimais por problema de precisão, por exemplo 0,9+0,1 == 0,99999...
+    function round (num1,num2) {
+        if(num1==''){
+            return num2;
+        }else{
+            var ArrayNum1 = num1.split(".");
+            var ArrayNum2 = num2.split(".");
+            var decimal = parseInt(ArrayNum1[1])+parseInt(ArrayNum2[1]);
+            if(decimal<1000){
+                    return parseInt(ArrayNum1[0])+parseInt(ArrayNum2[0])+'.'+decimal;
+            }    
+            else
+                return parseInt((decimal.toString())[0])+parseInt(ArrayNum1[0])+'.'+ (decimal.toString()).slice(1,4);
+        } 
+        
     }
+    
     function combinatoria (n,p){
         return fatorial(n)/(fatorial(p)*fatorial(n-p));
     }
