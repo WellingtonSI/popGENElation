a0dd092a9e086a163c3d76b82e993b7d7d0b25c4
WellingtonSI <12ribe@gmail.com>
2022-08-03 11:47:11 -0300

    meta head

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,6 +1,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
   <meta name="csrf-token" content="{{ csrf_token() }}" />
   <title>Pop - @yield('htmlheader_title', 'Your title here')</title>
   <!-- Tell the browser to be responsive to screen width -->
@@ -16,6 +17,7 @@
   <link rel="stylesheet" href="{{asset('dist/css/style.css')}}">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  
 </head>
 
 
