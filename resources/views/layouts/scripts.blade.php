b2e3954ec37d32eb42ccdced28794aa3f327dbbd
WellingtonSI <12ribe@gmail.com>
2022-08-08 10:41:14 -0300

    reitadado asset

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,22 +1,17 @@
 <!-- jQuery -->
-<script src="{{asset('plugins/jquery/jquery.min.js') }}"></script>
+<script src="/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 
  <!-- overlayScrollbars -->
- <script src="{{asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+ <script src="/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 
  <!-- AdminLTE App -->
-<script src="{{asset('dist/js/adminlte.min.js') }}"></script>
+<script src="/dist/js/adminlte.min.js"></script>
 
 <!-- AdminLTE for demo purposes -->
-<script src="{{asset('dist/js/demo.js') }}"></script>
+<script src="/dist/js/demo.js"></script>
 <!-- sweetalert2 -->
-<link rel="stylesheet" href="{{ asset('plugins/sweetalert2/sweetalert2.min.css') }}">
-<script src="{{ asset('plugins/sweetalert2/sweetalert2.all.min.js') }}"></script>
+<link rel="stylesheet" href="/plugins/sweetalert2/sweetalert2.min.css">
+<script src="/plugins/sweetalert2/sweetalert2.all.min.js"></script>
 
-<script>
-    window.Laravel = {!! json_encode([
-        'csrfToken' => csrf_token(),
-    ]) !!};
-</script>
